633f36c94b23ae204a9cfc9af193a8b2f65084ab
一ཫ ꕥ Sɩƴʌɱ ꕥ ཀ ཐི༏ཋྀ <124901343+Siyam-hacker-bd@users.noreply.github.com>
2024-10-20 21:42:15 +0600

    Add files via upload

---
+++ src/agent/workersai.ts
@@ -0,0 +1,107 @@
+import type { AgentUserConfig } from '../config/env';
+import type { SseChatCompatibleOptions } from './request';
+import type { ChatAgent, ChatStreamTextHandler, HistoryItem, ImageAgent, LLMChatParams } from './types';
+import { isJsonResponse, requestChatCompletions } from './request';
+
+class WorkerBase {
+    readonly name = 'workers';
+    readonly run = async (model: string, body: any, id: string, token: string): Promise<Response> => {
+        return await fetch(
+            `https://api.cloudflare.com/client/v4/accounts/${id}/ai/run/${model}`,
+            {
+                headers: { Authorization: `Bearer ${token}` },
+                method: 'POST',
+                body: JSON.stringify(body),
+            },
+        );
+    };
+
+    readonly enable = (context: AgentUserConfig): boolean => {
+        return !!(context.CLOUDFLARE_ACCOUNT_ID && context.CLOUDFLARE_TOKEN);
+    };
+}
+
+export class WorkersChat extends WorkerBase implements ChatAgent {
+    readonly modelKey = 'WORKERS_CHAT_MODEL';
+
+    readonly model = (ctx: AgentUserConfig): string => {
+        return ctx.WORKERS_CHAT_MODEL;
+    };
+
+    private render = (item: HistoryItem): any => {
+        return {
+            role: item.role,
+            content: item.content,
+        };
+    };
+
+    readonly request = async (params: LLMChatParams, context: AgentUserConfig, onStream: ChatStreamTextHandler | null): Promise<string> => {
+        const { message, prompt, history } = params;
+        const id = context.CLOUDFLARE_ACCOUNT_ID;
+        const token = context.CLOUDFLARE_TOKEN;
+        const model = context.WORKERS_CHAT_MODEL;
+        const url = `https://api.cloudflare.com/client/v4/accounts/${id}/ai/run/${model}`;
+        const header = {
+            Authorization: `Bearer ${token}`,
+        };
+
+        const messages = [...(history || []), { role: 'user', content: message }];
+        if (prompt) {
+            messages.unshift({ role: context.SYSTEM_INIT_MESSAGE_ROLE, content: prompt });
+        }
+
+        const body = {
+            messages: messages.map(this.render),
+            stream: onStream !== null,
+        };
+
+        const options: SseChatCompatibleOptions = {};
+        options.contentExtractor = function (data: any) {
+            return data?.response;
+        };
+        options.fullContentExtractor = function (data: any) {
+            return data?.result?.response;
+        };
+        options.errorExtractor = function (data: any) {
+            return data?.errors?.[0]?.message;
+        };
+        return requestChatCompletions(url, header, body, onStream, null, options);
+    };
+}
+
+export class WorkersImage extends WorkerBase implements ImageAgent {
+    readonly modelKey = 'WORKERS_IMAGE_MODEL';
+
+    readonly model = (ctx: AgentUserConfig): string => {
+        return ctx.WORKERS_IMAGE_MODEL;
+    };
+
+    readonly request = async (prompt: string, context: AgentUserConfig): Promise<Blob> => {
+        const id = context.CLOUDFLARE_ACCOUNT_ID;
+        const token = context.CLOUDFLARE_TOKEN;
+        if (!id || !token) {
+            throw new Error('Cloudflare account ID or token is not set');
+        }
+        const raw = await this.run(context.WORKERS_IMAGE_MODEL, { prompt }, id, token);
+        if (isJsonResponse(raw)) {
+            const { result } = await raw.json();
+            const image = result?.image;
+            if (typeof image !== 'string') {
+                throw new TypeError('Invalid image response');
+            }
+            return base64StringToBlob(image);
+        }
+        return await raw.blob();
+    };
+}
+
+async function base64StringToBlob(base64String: string): Promise<Blob> {
+    try {
+        const { Buffer } = await import('node:buffer');
+        const buffer = Buffer.from(base64String, 'base64');
+        return new Blob([buffer], { type: 'image/png' });
+    } catch {
+        const uint8Array = Uint8Array.from(atob(base64String), c => c.charCodeAt(0));
+        return new Blob([uint8Array], { type: 'image/png' });
+    }
+}
---
+++ vite.config.ts
@@ -0,0 +1,72 @@
+import type { LibraryFormats, Plugin } from 'vite';
+import * as path from 'node:path';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import cleanup from 'rollup-plugin-cleanup';
+import nodeExternals from 'rollup-plugin-node-externals';
+import { defineConfig } from 'vite';
+import checker from 'vite-plugin-checker';
+import dts from 'vite-plugin-dts';
+import { createDockerPlugin } from './scripts/plugins/docker';
+import { createVersionPlugin, versionDefine } from './scripts/plugins/version';
+
+const { BUILD_MODE } = process.env;
+const plugins: Plugin[] = [
+    nodeResolve({
+        preferBuiltins: true,
+    }),
+    cleanup({
+        comments: 'none',
+        extensions: ['js', 'ts'],
+    }),
+    checker({
+        typescript: true,
+    }),
+    nodeExternals(),
+];
+
+let entry: string;
+let outDir = 'dist';
+let fileName = 'index';
+let formats: LibraryFormats[] = ['es'];
+switch (BUILD_MODE) {
+    case 'plugins-page':
+        entry = 'src/plugins/interpolate.ts';
+        fileName = 'interpolate';
+        outDir = 'plugins/dist';
+        break;
+    case 'local':
+        entry = 'src/adapter/local/index.ts';
+        plugins.push(createDockerPlugin('dist'));
+        break;
+    case 'vercel':
+        entry = 'src/adapter/vercel/index.ts';
+        break;
+    case 'pack':
+        entry = 'src/index.ts';
+        formats = ['es', 'cjs'];
+        plugins.push(dts({
+            rollupTypes: true,
+        }));
+        break;
+    default:
+        entry = 'src/index.ts';
+        plugins.push(createVersionPlugin('dist'));
+        break;
+}
+
+export default defineConfig({
+    plugins,
+    build: {
+        target: 'esnext',
+        lib: {
+            entry: path.resolve(__dirname, entry),
+            fileName,
+            formats,
+        },
+        minify: false,
+        outDir,
+    },
+    define: {
+        ...versionDefine,
+    },
+});
